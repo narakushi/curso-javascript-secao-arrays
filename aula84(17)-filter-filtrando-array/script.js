04b2927d0b83b8fa045a4473b0f12036928737bf
narakushi <taina.santana1305@gmail.com>
2025-02-21 13:59:42 -0300

    aula 84 (17) - filter

--- aula84(17)-filter-filtrando-array/script.js
+++ aula84(17)-filter-filtrando-array/script.js
@@ -1,8 +1,38 @@
 // Filter, map, reduce
 
+// Retorne os números maiores que 10
 
-//Retorne os numeros maiores que 10
+const numeros = [5, 50, 90, 1, 2, 3, 4, 5, 5, 8, 7, 11, 15, 22, 27];
+let newArray = [];
+/*
+for (let numero of numeros){
+  if(numero > 10){
+    newArray.push(numero);
+  }
+}
 
+console.log(newArray);*/
 
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+/** retornando com filter */
+
+function callbackFilter(valor /*indice, array*/) {
+  /*if(valor > 10) {
+    return true;
+  }
+  else {
+    return false;
+  }*/
+
+  return valor > 10;
+}
+
+//const numerosFiltrados = numeros.filter(callbackFilter);
+//função sintetizada
+/*const numerosFiltrados = numeros.filter((valor, indice, array) => {
+  console.log(valor, indice, array);
+  return valor > 10;
+});*/
+
+const numerosFiltrados = numeros.filter(valor => valor > 10);
+console.log(numerosFiltrados);
 
