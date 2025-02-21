958714db79a615854bf501d6d9ba3fef673d4791
narakushi <taina.santana1305@gmail.com>
2025-02-21 14:04:06 -0300

    trabalhando com filtragem em objetos

--- aula84(17)-filter-filtrando-array/script.js
+++ aula84(17)-filter-filtrando-array/script.js
@@ -1,4 +1,5 @@
 // Filter, map, reduce
+// Filter -> sempre ira retornar um array, com a mesma quantidade de elementos ou menos
 
 // Retorne os números maiores que 10
 
@@ -35,4 +36,19 @@ function callbackFilter(valor /*indice, array*/) {
 
 const numerosFiltrados = numeros.filter(valor => valor > 10);
 console.log(numerosFiltrados);
+
+/** Segundo exemplo */
+
+// Retorne as pessoas que tem nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 abos
+// Retorne as pessoas cujo nome termina com "a";
+
+const pessoas = [
+  { nome: 'Luiz', idade: 62 },
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduardo', idade: 55 },
+  { nome: 'Letícia', idade: 19 },
+  { nome: 'Rosana', idade: 32 },
+  { nome: 'Wallace', idade: 47 }
+];
 
